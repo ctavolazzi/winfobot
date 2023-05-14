d95e6fcf4c416eb006a32cac67360077b7c878cf
Christopher Tavolazzi <ctavolazzi@gmail.com>
2023-05-13 23:55:24 -0700

    v0.1 Browser Client

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from aigent import AIGent
 import logging
 import os
@@ -9,17 +9,29 @@ import openai
 import requests
 
 app = Flask(__name__)
-winfo_bot = AIGent()
+agent = AIGent()
 
 @app.route('/')
 def home():
-    return "WinfoBot is running!"  # Return a simple string to indicate that the bot is running
+    return render_template('index.html')  # Assuming you have a template named index.html
 
-@app.route('/winfo', methods=['POST'])
+@app.route('/chat', methods=['POST'])
 def chat():
-    user_message = request.json['message']
-    bot_message = winfo_bot.chat(user_message)
-    return {'message': f"{winfo_bot.name_first}: {bot_message}"}
+    user_message = request.form['message']
+    agent_message = agent.chat(user_message)
+    return {'message': f"{agent.name_first}: {agent_message}"}
+
+@app.route('/logs', methods=['GET'])
+def list_logs():
+    log_files = os.listdir('path/to/your/log/directory')
+    return {'logs': log_files}
+
+@app.route('/logs/<log_file>', methods=['GET'])
+def get_log(log_file):
+    with open(f'path/to/your/log/directory/{log_file}', 'r') as file:
+        log_content = file.readlines()
+    return {'log_content': log_content}
+
 
 if __name__ == '__main__':
     # Get the absolute path of the current directory
